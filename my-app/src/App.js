acd83d10fa91517bde103b13a56f5db7d3a1f471
Liam Murray <lmurray298@Liams-MacBook-Pro.local>
2019-05-11 12:47:59 -0400

    got react files

---
+++ my-app/src/App.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import GoogleMapReact from 'react-google-maps';
+ 
+const AnyReactComponent = ({ text }) => <div>{text}</div>;
+ 
+class SimpleMap extends Component {
+  static defaultProps = {
+    center: {
+      lat: 59.95,
+      lng: 30.33
+    },
+    zoom: 11
+  };
+ 
+  render() {
+    return (
+      // Important! Always set the container height explicitly
+      <div style={{ height: '100vh', width: '100%' }}>
+        <GoogleMapReact
+          bootstrapURLKeys={{ key:"AIzaSyAZh-sq69yI6rpXdlr0iBOchlk3O-G21mc" }}
+          defaultCenter={this.props.center}
+          defaultZoom={this.props.zoom}
+        >
+          <AnyReactComponent
+            lat={59.955413}
+            lng={30.337844}
+            text="My Marker"
+          />
+        </GoogleMapReact>
+      </div>
+    );
+  }
+}
+ 
+export default SimpleMap;
+
+
+
