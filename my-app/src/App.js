e42719ad886256a3091905f5039dba8b12b5bf39
Liam Murray <lmurray298@Liams-MacBook-Pro.local>
2019-05-15 20:14:56 -0400

    added map

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,38 +1,49 @@
-import React, { Component } from 'react';
-import GoogleMapReact from 'react-google-maps';
- 
-const AnyReactComponent = ({ text }) => <div>{text}</div>;
- 
-class SimpleMap extends Component {
-  static defaultProps = {
-    center: {
-      lat: 59.95,
-      lng: 30.33
-    },
-    zoom: 11
-  };
- 
+import React, { Component } from "react";
+import {Map, InfoWindow, Marker, GoogleApiWrapper} from "google-maps-react";
+
+const style ={
+  width: "70%",
+  height: "80%"
+
+};
+
+
+class App extends Component {
+  
   render() {
     return (
-      // Important! Always set the container height explicitly
-      <div style={{ height: '100vh', width: '100%' }}>
-        <GoogleMapReact
-          bootstrapURLKeys={{ key:"AIzaSyAZh-sq69yI6rpXdlr0iBOchlk3O-G21mc" }}
-          defaultCenter={this.props.center}
-          defaultZoom={this.props.zoom}
+      <div className="App">
+        <header className="App-header">
+        <h1 className="App-title"> Emergency Map</h1>
+        </header>
+
+        <Map google={this.props.google} zoom={14}
+        style = {style}
+        justify= "left"
+        initialCenter={{
+          lat: 42.3601,
+          lng: -71.0589
+        }}
         >
-          <AnyReactComponent
-            lat={59.955413}
-            lng={30.337844}
-            text="My Marker"
-          />
-        </GoogleMapReact>
+        <Marker onClick={this.onMarkerClick}
+              name={'Current location'} />
+ 
+        <InfoWindow onClose={this.onInfoWindowClose}>
+           
+        </InfoWindow>
+
+      </Map>
+        
       </div>
     );
   }
 }
- 
-export default SimpleMap;
 
+
+
+export default GoogleApiWrapper({
+  apiKey: ("AIzaSyAZh-sq69yI6rpXdlr0iBOchlk3O-G21mc")
+})(App)
+ 
 
 
