fbb4a6dc251d81133e6c771090826ffad642ae0a
Liam Murray <lmurray298@Liams-MacBook-Pro.local>
2019-06-03 19:41:00 -0400

    test push

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -1,67 +1,31 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
-import Googmap from "../Googmap";
 
 
-const routes = [
-  {
-    path: "/",
-    exact: true,
-    sidebar: () => <div>home!</div>,
-    main: () => <h2>Home</h2>
-  },
-  {
-    path: "/google",
-    sidebar: () => <div>googlemap</div>,
-    main: () => <h2><Googmap/></h2>
+class Nav extends React.Component {
+  render() {
+      return (
+          <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+          <a className="navbar-brand" href="/"> Durham Emergency Map</a>
+          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+  
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav mr-auto">
+              
+              <li className="nav-item">
+                <a className="nav-link" href="/">Logout</a>
+              </li>
+              
+            </ul>
+            <form className="form-inline my-2 my-lg-0">
+              <input className="form-control mr-sm-1 text-center" type="search" placeholder="Street Filter" aria-label="Search" />
+             
+            </form>
+          </div>
+        </nav>
+      )
   }
-];
-
-function Nav() {
-  return (
-    <Router>
-      <div style={{ display: "flex" }}>
-        <div
-          style={{
-            padding: "10px",
-            width: "10%",
-            background: "#f0f0f0"
-          }}
-        >
-          <ul style={{ listStyleType: "none", padding: 0 }}>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/google">Googlemap</Link>
-            </li>
-          </ul>
-
-          {routes.map((route, index) => (
-            
-            <Route
-              key={index}
-              path={route.path}
-              exact={route.exact}
-              component={route.sidebar}
-            />
-          ))}
-        </div>
-
-        <div style={{ flex: 1, padding: "10px" }}>
-          {routes.map((route, index) => (
-            
-            <Route
-              key={index}
-              path={route.path}
-              exact={route.exact}
-              component={route.main}
-            />
-          ))}
-        </div>
-      </div>
-    </Router>
-  );
 }
 
 export default Nav;
